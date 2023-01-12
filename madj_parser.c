b8c780b2a327f446dfb50dc0f773e54c80626f18
Alexandre Maheux <alexandre.maheux@ens.uvsq.fr>
2023-01-12 20:45:50 +0100

    Modify hash function without collisions
    The hash function come from http://burtleburtle.net/bob/hash/evahash.html
    and it is modified a little bit

--- madj_parser.c
+++ madj_parser.c
@@ -7,11 +7,7 @@
 #define ASIN_LENGTH 10
 #define MAX_LENGTH 233667
 #define MAX_LINE 9430088
-
-struct hash_item {
-	uint64_t value;
-	char *asin;
-};
+#define MAX_HASH 16777216 //2^24 > MAX_LINE
 
 void load_asin(char **asin_tab)
 {
@@ -23,7 +19,7 @@ void load_asin(char **asin_tab)
 	FILE *f = fopen("id_product_list.csv", "r");
 	if(!f)
 	{
-		perror("fopen error");
+		perror("id_product_list.csv");
 		exit(EXIT_FAILURE);
 	}
 
@@ -31,8 +27,8 @@ void load_asin(char **asin_tab)
 
 	for(uint64_t i = 0; i < MAX_LINE; ++i)
 	{
-		if(i%100000==0)
-			printf("%lu\n", i);
+		// if(i%100000==0)
+		// 	printf("%lu\n", i);
 
 		fgets(buffer, 16, f);
 		strncpy(asin_tab[i], buffer, ASIN_LENGTH+1);
@@ -44,75 +40,62 @@ void load_asin(char **asin_tab)
 	free(buffer);
 }
 
-int hash1(char *str)
+int hash(char *str)
 {
-	uint64_t capacity = 16777216; //2^24 > MAX_LINE
-	uint64_t sum = 0, mul = 0;
-	for(int i = 0; i < 10; ++i)
-	{
-		mul = ((i&3)==0) ? 1 : mul * 256;
-		sum += str[i] * mul;
-	}
+	int a = 0x9e3779b9;
+	int b = 0x9e3779b9;
+	int c = 10;
+	
+   	a += str[0] + (str[1] << 8) + (str[2] << 16) + (str[3] << 24);
+   	b += str[4] + (str[5] << 8) + (str[6] << 16) + (str[7] << 24);
+   	c += (str[8] << 8) + (str[9] << 16);
 
-	return sum & (capacity-1); //1981 collisions
-}
+   	a -= b; a -= c; a ^= c >> 13;
+	b -= c; b -= a; b ^= a << 8;
+	c -= a; c -= b; c ^= b >> 13;
+  	a -= b; a -= c; a ^= c >> 12;
+  	b -= c; b -= a; b ^= a << 16;
+  	c -= a; c -= b; c ^= b >> 5;
+ 	a -= b; a -= c; a ^= c >> 3;
+  	b -= c; b -= a; b ^= a << 10;
+  	c -= a; c -= b; c ^= b >> 15;
 
-int hash2(char *str)
-{
-	uint64_t capacity = 16777216; //2^24 > MAX_LINE
-	uint64_t h = 0, g = 0;
-	for(int i = 0; i < 10; ++i)
-	{
-		g = h & 0xf8000000;
-		h <<= 5;
-		h ^= g >> 27;
-		h ^= str[i];
-	}
-
-	return h & (capacity-1); //404 collisions
+   	return c & (MAX_HASH-1);
 }
 
 // SMLRBIMX03 -> 534D4C5242494D583033
 
 void detect_collisions(char **asin_tab)
 {
+	// FILE *fw = fopen("output_hash", "w");
+	// if(!fw)
+	// {
+	// 	perror("output_hash");
+	// 	exit(EXIT_FAILURE);
+	// }
+
 	uint64_t prev = 0;
 	uint64_t next = 0;
 	uint64_t collide = 0;
 	for(uint64_t i = 0; i < MAX_LINE; i++)
 	{
-		if(i%100000==0)
-			printf("%lu\n", i);
+		// if(i%100000==0)
+		// 	printf("%lu\n", i);
 
-		next = hash2(asin_tab[i]);
+		next = hash(asin_tab[i]);
+		// fprintf(fw ,"%ld\n", next);
 		if(prev == next)
 			collide++;
 		prev = next;
 	}
 
-	printf("Il y a eu %ld collisions\n", collide);
-}
+	// fclose(fw);
 
-void create_hash_map(struct hash_item *hash_map, char **asin_tab)
-{	
-	uint64_t j = 0;
-	for(uint64_t i = 1; i < MAX_LINE; ++i)
-	{
-		uint64_t k = 0;
-		j = hash2(asin_tab[i]);
-		if(!strcmp(hash_map[j].asin, ""))
-		{
-			k = 1;
-			while(hash_map[k].value != 0) k++;
-		}
-		hash_map[j+k] = (struct hash_item) {i, asin_tab[i]};
-	}
+	printf("Il y a eu %ld collisions\n", collide);
 }
 
 /*
 remplacer asin_tab par le tableau de hachage, gain de place, ou pas ça peut peut-etre encore servir
-rajouter le str dans item pour vérifier si à l'appel on tombe bien sur la bonne case
-arriver à gérer les collisions putain
 */
 
 int main(int argc, char **argv)
@@ -169,8 +152,8 @@ int main(int argc, char **argv)
 	for(uint64_t i = 0; i < MAX_LINE; ++i)
 	{
 		free(asin_tab[i]);
-		if(i%1000000==0)
-			printf("%ld\n", i);
+		// if(i%1000000==0)
+		// 	printf("%ld\n", i);
 	}
 	free(asin_tab);
 	printf("Libéré\n");
