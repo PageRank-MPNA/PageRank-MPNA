e034cefd3371d918990dcadd6b189ad27f9f270f
Alexandre Maheux <alexandre.maheux@ens.uvsq.fr>
2023-01-13 11:39:21 +0100

    Add measures of time of functions
    Elapsed time for allocation, fill array, reading and store and
    freeing.

--- madj_parser.c
+++ madj_parser.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
 
 #define ALIGN_SIZE 512
 #define ASIN_LENGTH 10
@@ -14,8 +15,6 @@ void load_asin(char **asin_tab)
 	char *buffer = aligned_alloc(16, sizeof(char)*16);
 	memset(buffer, 0, sizeof(char)*16);
 
-	printf("Buffer (load_asin) alloué et initialisé à zéro\n");
-
 	FILE *f = fopen("id_product_list.csv", "r");
 	if(!f)
 	{
@@ -23,15 +22,11 @@ void load_asin(char **asin_tab)
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Lecture de id_product_list.csv ...\n");
-
 	for(uint64_t i = 0; i < MAX_LINE; ++i)
 	{
 		fgets(buffer, 16, f);
 		strncpy(asin_tab[i], buffer, ASIN_LENGTH+1);
 	}
-
-	printf("Fermeture de ../correct_metadata.json ...\n");
 
 	fclose(f);
 	free(buffer);
@@ -40,33 +35,49 @@ void load_asin(char **asin_tab)
 void compute_hash_map(uint64_t *hash_tab, char **asin_tab)
 {
 	uint64_t paddr = (uint64_t) asin_tab;
-	uint64_t collide = 0;
+	// uint64_t collide = 0;
 
 	for(uint64_t i = 0; i < MAX_LINE; i++)
 	{
 		uint64_t addr = (uint64_t) &asin_tab[i];
 		uint64_t h = addr - paddr; // Hash Function
-		if(hash_tab[h] != 0)
-			collide++;
+		// if(hash_tab[h] != 0)
+		// 	collide++;
 		hash_tab[h] = i;
 	}
 
-	printf("Il y a eu %ld collisions\n", collide);
+	// printf("Il y a eu %ld collisions\n", collide);
+}
+
+double elapsed(struct timespec start, struct timespec stop)
+{
+	return (double) stop.tv_sec - (double) start.tv_sec + ((double) stop.tv_nsec - (double) start.tv_nsec)*1e-9;
 }
 
 int main(int argc, char **argv)
 {
+	struct timespec global_start, global_stop;
+	struct timespec local_start, local_stop;
+
+	clock_gettime(CLOCK_MONOTONIC_RAW, &global_start);
+
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_start);
 	char **asin_tab = aligned_alloc(ALIGN_SIZE, sizeof(char *) * MAX_LINE);
 	for(int64_t j = 0; j < MAX_LINE; ++j)
 		asin_tab[j] = aligned_alloc(ALIGN_SIZE, sizeof(char)*(ASIN_LENGTH + 1));
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_stop);
 
-	printf("Tableau des id produits alloué\n");
+	printf("Allocation product id array : 	  %lf secondes\n", elapsed(local_start, local_stop));
 
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_start);
 	load_asin(asin_tab);
-	printf("Tableau des id produits chargé\n");
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_stop);
+
+	printf("Fill product id array : 	  %lf secondes\n", elapsed(local_start, local_stop));
 
 	uint64_t hash_length = (uint64_t) &asin_tab[MAX_LINE-1] - (uint64_t) asin_tab;
 
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_start);
 	uint64_t *hash_tab = aligned_alloc(ALIGN_SIZE, sizeof(uint64_t) * hash_length);
 	if(!hash_tab)
 	{
@@ -74,57 +85,65 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 	memset(hash_tab, 0, sizeof(uint64_t) * hash_length);
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_stop);
 
-	printf("Table de hachage de taille %ld alloué\n", hash_length);
+	printf("Allocation and init hash table :  %lf secondes\n", elapsed(local_start, local_stop));
 
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_start);
 	compute_hash_map(hash_tab, asin_tab);
-	printf("Table de hachage chargé\n");
-
-	// for(int i = 0; i < 100; ++i)
-	// 	printf("%s\n", asin_tab[i]);
-
-	// FILE *f = fopen("../correct_metadata.json", "r");
-	// if(!f)
-	// 	return printf("Cannot open correct_metadata.json\n"), 1;
-
-	// // uint64_t i = 0;
-	// char *buffer = aligned_alloc(ALIGN_SIZE, sizeof(char)*MAX_LENGTH);
-	// memset(buffer, 0, sizeof(char)*MAX_LENGTH);
-
-	// while(!feof(f))
-	// {
-	// 	if(i%100000==0)
-	// 	{
-	// 		printf("%lu\n", i);
-	// 	}
-
-	// 	fgets(buffer, MAX_LENGTH, f);
-
-	// 	if(ferror(f))
-	// 	{
-    //         fprintf(stderr, "Reading error with code %d\n", errno);
-    //         break;
-    //     }
-
-    //     // result = strstr(buffer, "0006458718");
-    //     // if(result != NULL)
-    //     // 	printf("%c%c%c%c%c%c%c%c%c%c\n", buffer[10], buffer[11], buffer[12], buffer[13], buffer[14], buffer[15], buffer[16], buffer[17], buffer[18], buffer[19]);
-
-    //     i++;
-	// }
-
-	// free(buffer);
-	// fclose(f);
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_stop);
 	
-	printf("Libération de la table de hachage...\n");
+	printf("Fill hash table : 		  %lf secondes\n", elapsed(local_start, local_stop));
+
+	FILE *datafile = fopen("correct_metadata.json", "r");
+	if(!datafile)
+	{
+		perror("correct_metadata.json");
+		exit(EXIT_FAILURE);
+	}
+
+	char *buffer = aligned_alloc(ALIGN_SIZE, sizeof(char)*MAX_LENGTH);
+	memset(buffer, 0, sizeof(char)*MAX_LENGTH);
+
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_start);
+	for(uint64_t i = 0; i < MAX_LINE; ++i)
+	{
+		// if(i%100000==0)
+		// {
+		// 	printf("%lu\n", i);
+		// }
+
+		fgets(buffer, MAX_LENGTH, datafile);
+
+		if(ferror(datafile))
+		{
+            perror("Read correct_metadata.json");
+            exit(EXIT_FAILURE);
+        }
+
+        // result = strstr(buffer, "0006458718");
+        // if(result != NULL)
+        // 	printf("%c%c%c%c%c%c%c%c%c%c\n", buffer[10], buffer[11], buffer[12], buffer[13], buffer[14], buffer[15], buffer[16], buffer[17], buffer[18], buffer[19]);
+	}
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_stop);
+	printf("Read and store in buffer : 	  %lf secondes\n", elapsed(local_start, local_stop));
+
+	free(buffer);
+	fclose(datafile);
+
 	free(hash_tab);
 
-	printf("Libération du tableau des id produits...\n");
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_start);
 	for(uint64_t i = 0; i < MAX_LINE; ++i)
 		free(asin_tab[i]);
 	free(asin_tab);
+	clock_gettime(CLOCK_MONOTONIC_RAW, &local_stop);
+	printf("Free product id array : 	  %lf secondes\n", elapsed(local_start, local_stop));
 
-	printf("Libéré\n");
+	clock_gettime(CLOCK_MONOTONIC_RAW, &global_stop);
+
+	printf("------------------------------------------\n");
+	printf("Total elapsed time : 		  %lf secondes\n", elapsed(global_start, global_stop));
 
 	return 0;
 }
